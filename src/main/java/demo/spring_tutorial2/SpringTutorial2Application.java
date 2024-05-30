96ad4b7ee93d48f2803772936ec811133db9fb52
kkimsuhyton <wzxy2002@naver.com>
2024-05-30 23:04:03 +0900

    project init

---
+++ src/main/java/demo/spring_tutorial2/SpringTutorial2Application.java
@@ -0,0 +1,13 @@
+package demo.spring_tutorial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorial2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorial2Application.class, args);
+    }
+
+}
---
+++ src/test/java/demo/spring_tutorial2/SpringTutorial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.spring_tutorial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorial2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
