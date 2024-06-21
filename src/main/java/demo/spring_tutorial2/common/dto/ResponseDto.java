24703e0dea9ffd86afc5d4c72fbdafa1a3837483
jjim1004 <jjlim1004@naver.com>
2024-06-21 22:48:00 +0900

    feat: jjlim1004 test commit and push

---
+++ src/main/java/demo/spring_tutorial2/common/dto/ResponseDto.java
@@ -0,0 +1,11 @@
+package demo.spring_tutorial2.common.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseDto {
+    Object dto;
+    User user;
+}
---
+++ src/main/java/demo/spring_tutorial2/common/dto/User.java
@@ -0,0 +1,10 @@
+package demo.spring_tutorial2.common.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private String id;
+}
